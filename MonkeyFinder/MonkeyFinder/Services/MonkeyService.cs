06aebfebe1f76640cbd3b27a298bc8025c1a5b6f
Ingvar Derešivski <inde999@gmail.com>
2022-11-27 13:21:43 +0200

    BaseViewModelAndMonkeyService

---
+++ MonkeyFinder/MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,26 @@
+﻿using MonkeyFinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyFinder.Services
+{
+    public class MonkeyService
+    {
+        HttpClient httpClient;
+
+        public MonkeyService()
+        {
+            httpClient = new HttpClient();
+        }
+
+        List<Monkey> monkeyList = new();
+
+        public async Task<List<Monkey>> Getmonkeys()
+        {
+            return monkeyList;
+        }
+    }
+}
