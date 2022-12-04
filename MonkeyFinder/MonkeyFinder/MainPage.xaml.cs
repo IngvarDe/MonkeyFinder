c74056a0458410d4a10fef2af866cd812f99e061
Ingvar Derešivski <inde999@gmail.com>
2022-12-04 10:47:54 +0200

    MainPage

--- MonkeyFinder/MonkeyFinder/MainPage.xaml.cs
+++ MonkeyFinder/MonkeyFinder/MainPage.xaml.cs
@@ -1,12 +1,15 @@
-﻿namespace MonkeyFinder;
+﻿using MonkeyFinder.ViewModel;
+
+namespace MonkeyFinder;
 
 public partial class MainPage : ContentPage
 {
 	//int count = 0;
 
-	public MainPage()
+	public MainPage(MonkeysViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
 
 	//private void OnCounterClicked(object sender, EventArgs e)
