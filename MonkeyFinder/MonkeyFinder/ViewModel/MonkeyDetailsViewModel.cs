c2a0793f6be995c11f13892f6a385d60a0779b84
Ingvar Derešivski <inde999@gmail.com>
2022-12-27 20:35:30 +0200

    OpenMapCommand

--- MonkeyFinder/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ MonkeyFinder/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using MonkeyFinder.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,15 @@ namespace MonkeyFinder.ViewModel
     [QueryProperty("Monkey", "Monkey")]
     public partial class MonkeyDetailsViewModel : BaseViewModel
     {
-        public MonkeyDetailsViewModel()
-        {
 
+        IMap map;
+
+        public MonkeyDetailsViewModel
+            (
+                IMap map
+            )
+        {
+            this.map = map;
         }
 
         [ObservableProperty]
@@ -25,6 +32,27 @@ namespace MonkeyFinder.ViewModel
         async Task GoBackAsync()
         {
             await Shell.Current.GoToAsync("..");
+        }
+
+        [RelayCommand]
+        async Task OpenMapAsync()
+        {
+            try
+            {
+                await map.OpenAsync(Monkey.Latitude, Monkey.Longitude,
+                    new MapLaunchOptions
+                    {
+                        Name = Monkey.Name,
+                        NavigationMode = NavigationMode.None
+                    });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error!",
+                    $"Unable to open map: {ex.Message}"
+                    , "OK");
+            }
         }
     }
 }
